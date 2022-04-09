5e93f7926f67d6063f27a4a5b1d26ea1fc4abb49
sanyukta001 <sanyuktamishra911@gmail.com>
2022-04-09 20:33:32 +0530

    concept on pointers

---
+++ pointers.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    // int i = 0;
+    // int* pt;
+    // pt = (int*)malloc(sizeof(int)*5);
+    // for (i = 0; i < 5; i++)
+    // {
+    //     printf("Enter the element at %d ",i);
+    //     scanf("%d",&pt[i]);//*(pt+i)
+    // }
+    // printf("The elements are: ");
+    // for (i = 0; i < 5; i++)
+    // {
+    //     printf("%d ",*(pt+i));
+    // }
+    //2Darray;
+    // int** pt;
+    // pt = (int**)malloc(sizeof(int)*3);
+    
+    //     for (int i = 0; i < 3; i++)
+    //     {
+    //         *(pt+i) = (int*)malloc(sizeof(int)*2);
+    //     }
+    //     for (int i = 0; i < 3; i++)
+    //    {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         printf("Enter the value at %d%d ",i,j);
+    //         scanf("%d",&pt[i][j]);//*(*(pt+i)+j)
+    //     }
+        
+    //    }
+    // printf("The values are\n");
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 2; j++)
+    //     {
+    //         printf("%d ",pt[i][j]);
+    //     }
+    //     printf("\n");    
+    // }
+     return 0;
+}
