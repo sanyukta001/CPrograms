23a74e6352f6924b3aa30442b388f9eb9f02a285
sanyukta001 <sanyuktamishra911@gmail.com>
2022-04-09 23:25:02 +0530

    cyclicswapping

---
+++ cyclicswap.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+// #include<stdlib.h>
+
+void CyclicSwap(int* n1,int* n2,int* n3)
+{
+    int temp = *n1;
+    *n1 = *n3;
+    *n3 = *n2;
+    *n2 = temp;  
+}
+int main()
+{
+    int a;
+    int b;
+    int c;
+    printf("Enter the value of a,b and c respectively\n");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("The vales before swapping:\na = %d\nb = %d\nc = %d\n ",a,b,c);
+    CyclicSwap(&a,&b,&c);
+    printf("\nThe vales after swapping:\na = %d\nb = %d\nc = %d\n ",a,b,c);
+    return 0;
+}
