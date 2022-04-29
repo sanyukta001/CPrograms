bb3b2dc768a991d01b8dae0ab0af2f3d97a9e7f4
sanyukta001 <sanyuktamishra911@gmail.com>
2022-04-29 22:45:06 +0530

    DSA(linked list)

---
+++ llm.c
@@ -0,0 +1,23 @@
+#include "ll.h"
+
+int main()
+{
+    node* head = NULL;
+
+    append(&head,1);
+    append(&head,2);
+    append(&head,3);
+    append(&head,4);
+    append(&head,5);
+    append(&head,6);
+
+    traverse(head);
+    delete_node(&head,3);
+    printf("After deletion:\n");
+    traverse(head);
+    add_node(&head,3,3);
+    printf("After addition:\n");
+    traverse(head);
+
+    return 0;
+}
