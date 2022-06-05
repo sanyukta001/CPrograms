12ff90d7fa299b1655f4643d0cb48c6a77843d21
sanyukta001 <sanyuktamishra911@gmail.com>
2022-06-05 18:18:58 +0530

    queue functions

---
+++ Queue/queue.c
@@ -0,0 +1,60 @@
+#include "queue.h"
+void init(Queue* line,int size)
+{
+    line->rear = -1;
+    line->front = -1;
+    line->arr = (int*)malloc(size*sizeof(int));
+    line->N = size;
+}
+int isempty(Queue* line)
+{
+    if(line->rear == -1 && line->front == -1)
+        return 1;
+    else
+        return 0;
+}
+int isfull(Queue* line)
+{
+    if(line->rear == (line->N)-1)
+        return 1;
+    else
+        return 0;
+}
+void enqueue(Queue* line, int value)
+{
+    if(isfull(line))
+    {
+        printf("Que is full\n");
+        return;
+    }
+    if(isempty(line))
+    {
+        line->front = 0;
+    }
+    line->rear++;
+    line->arr[line->rear] = value;
+}
+int dequeue(Queue* line)
+{
+    if(isempty(line))
+    {
+        printf("Que is empty\n");
+        return -9999;
+    }
+    int temp = line->arr[line->front];
+    line->front++;
+    return temp;
+}
+void display(Queue* line)
+{
+    int i;
+    if(isempty(line))
+    {
+        printf("Que is empty\n");
+        return;
+    }
+    for(i = line->front;i <= line->rear; i++)
+    {
+        printf("%d ",line->arr[i]);
+    }
+}
